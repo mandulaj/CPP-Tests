bf29fd14fad4130ed5b42bf24ac8d17d4bf58708
Jakub Mandula <jakub.aludnam@gmail.com>
2015-01-07 21:32:45 +0100

    classes are fun

---
+++ Class/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int x[5] = {1,2,3,4,5};
+    cout << x[5];
+    return 0;
+}
---
+++ Class/test.h
@@ -0,0 +1,14 @@
+#ifndef TEST_H
+#define TEST_H
+
+
+class test
+{
+    public:
+        /** Default constructor */
+        test(ee);
+    protected:
+    private:
+};
+
+#endif // TEST_H
